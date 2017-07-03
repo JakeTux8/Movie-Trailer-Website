6cafa50d7b538a961d936667ec93c5c7188558f1
JakeTux8 <james.rouse2003@gmail.com>
2017-07-03 11:15:29 -0400

    Update media.py
    Code updated to conform to PEP8 style guide.

--- media.py
+++ media.py
@@ -1,18 +1,22 @@
-import webbrowser #module needed to open url links in browser
+import webbrowser  # module needed to open url links in browser
+
 
 class Movie():
 
-    """"This class allows the creation of movie objects that include movie title, synopsis, poster, and trailer"""
-    
-    #Movie class constructor.  Four arguments besides self are needed to initialize: movie title, movie synopsis, poster image url, and youtube url.
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """"This class allows the creation of movie objects that include movie
+    title, synopsis, poster, and trailer"""
+
+    # Movie class constructor.
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
 
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Function to show movie trailer
+    # Function to show movie trailer
     def show_trailer(self):
 
-        webbrowser.open(self.trailer_youtube_url)  #opens youtube url in system's default browser
+        # opens youtube url in system's default browser
+        webbrowser.open(self.trailer_youtube_url)
